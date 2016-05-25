2bebb22cd513a5568b4ac5c07a7eb2a845545d00
Tom Manion <thomaswmanion@gmail.com>
2016-05-24 19:43:11 -0700

    Adding Akersh

--- sell.sh
+++ sell.sh
@@ -1,2 +1,3 @@
 #Sell stocks owned based on the isReadyToSell function of the SellSymbol class.
 ts-node seller/index.ts --username='tomskytwo' --password='Bigapples1!'
+ts-node seller/index.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
