9ed0d78946343a8fdeed2121b097597000b82dbd
Tom Manion <thomaswmanion@gmail.com>
2016-05-25 20:47:24 -0700

    Adding log statement

--- shared/yahoo-price.ts
+++ shared/yahoo-price.ts
@@ -12,6 +12,7 @@ export async function getLastNPrices(symbol: string, date: Date, n: number): Pro
             let url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22${symbol}%22%20and%20startDate%20%3D%20%222016-05-15%22%20and%20endDate%20%3D%20%22${formattedDate}%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 
             req.get(url, (err, response, body) => {
+                console.log(body);
                 body = JSON.parse(body);
                 let results: YahooQueryResult[] = body.query.results.quote.slice(0, n);
                 resolve(results);
