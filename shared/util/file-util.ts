40ff62118d8a8c8f3a68d9cff20c417f8c272d4b
Tom Manion <thomaswmanion@gmail.com>
2016-05-27 12:01:01 -0500

    Adding username hash to sell stats

--- shared/util/file-util.ts
+++ shared/util/file-util.ts
@@ -1,10 +1,23 @@
 import * as path from 'path';
-
+import * as yargs from 'yargs';
+let argv = yargs.argv;
+let username = argv.username;
 let userHome = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']; 
 
 export class FileUtil {
     static resultsFile:string = path.join(userHome, 'results.json');
     static buyFile:string = path.join(userHome, 'buy.json');
-    static sellStatsFile:string = path.join(userHome, 'sell-stats.json');
+    static sellStatsFile:string = path.join(userHome, `sell-stats-${hashCode(username)}.json`);
     static svmFile:string = path.join(userHome, 'svm.json');
+}
+
+function hashCode(s) {
+    var hash = 0, i, chr, len;
+    if (s.length === 0) return hash;
+    for (i = 0, len = s.length; i < len; i++) {
+        chr = s.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0;
+    }
+    return Math.abs(hash);
 }
