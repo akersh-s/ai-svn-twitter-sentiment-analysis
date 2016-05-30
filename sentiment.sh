79aa4a9c733d88816ceda131a6e06ed8b1fe6c1d
Tom Manion <thomaswmanion@gmail.com>
2016-05-30 15:51:24 -0500

    Adding today parameter of start date

--- sentiment.sh
+++ sentiment.sh
@@ -1,10 +1,12 @@
 rm -rf ~/results.json
 rm -rf ~/svm.json
 
+today=`date`
+
 #Do Sentiment Analysis on each symbol from Twitter
 while read symbol keywords; do
-    ts-node sentiment-search --symbol="$symbol" --keywords="$keywords" --debug
-    echo "ts-node sentiment-search --symbol=$symbol --keywords=$keywords --debug"
+    ts-node sentiment-search --symbol="$symbol" --keywords="$keywords" --today="$today" --debug
+    echo "ts-node sentiment-search --symbol=$symbol --keywords=$keywords" --today="$today" --debug
 done < "sentiment-search/stocks"
 
 #Determine which to buy and save symbols to buy.json
