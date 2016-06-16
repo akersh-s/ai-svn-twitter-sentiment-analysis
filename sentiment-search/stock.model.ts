274b235e003e219af04f271027a41d0cd65d51ab
Tom Manion <thomaswmanion@gmail.com>
2016-06-16 17:02:31 -0500

    Removing OR from empty search terms

--- sentiment-search/stock.model.ts
+++ sentiment-search/stock.model.ts
@@ -2,6 +2,9 @@ export class Stock {
     constructor(public symbol: string, public keywords: string) {}
     
     get q():string {
+        if (!this.keywords || this.keywords.trim() === '') {
+            return this.symbol;
+        }
         let searchTerms = [this.symbol, this.keywords];
         let args:string = searchTerms.join(' OR ');
         return args;
