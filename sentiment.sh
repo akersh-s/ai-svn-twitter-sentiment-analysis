1fb5b60a9f2b568325f0191c9447846838058a56
Tom Manion <thomaswmanion@gmail.com>
2016-06-24 17:50:31 -0500

    Changing for yesterdays run

--- sentiment.sh
+++ sentiment.sh
@@ -2,8 +2,8 @@ rm -rf ~/results.json
 rm -rf ~/svm.json
 rm -rf ~/buy.json
 
-today=`date`
-
+#today=`date`
+today='06/23/2016'
 ts-node sentiment --today="$today" --debug
 
 #Determine which to buy and save symbols to buy.json
