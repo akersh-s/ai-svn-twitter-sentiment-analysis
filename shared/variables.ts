0eb0f9489c6ddd243bfb2a603c134a928f16de56
Tom Manion <thomaswmanion@gmail.com>
2016-08-16 17:49:46 -0500

    Setting max svm data to 26,000 with new prediction of price

--- shared/variables.ts
+++ shared/variables.ts
@@ -24,7 +24,7 @@ export class Variables {
     static kernelType: string = 'RBF';
     static svmType: string = 'C_SVC';
 
-    static maxSvmData: number = 28000;
+    static maxSvmData: number = 26000;
 
     static includeFundamentals():boolean {
         return Variables.includeHighChange || Variables.includeLowChange || Variables.includeVolumeChange;
