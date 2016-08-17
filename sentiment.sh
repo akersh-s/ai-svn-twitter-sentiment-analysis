367aaa07a4a44816aec9618d5bc96476bf6edaac
Tom Manion <thomaswmanion@gmail.com>
2016-08-17 13:19:27 -0500

    Changing today to yesterday

--- sentiment.sh
+++ sentiment.sh
@@ -2,6 +2,6 @@ rm -rf ~/results.json
 rm -rf ~/svm.json
 rm -rf ~/buy.json
 
-today=`date`
+today=`ts-node shared/get-yesterday`
 ts-node sentiment --today="$today" --debug
 ts-node process/run-process-results --today="$today" --debug
