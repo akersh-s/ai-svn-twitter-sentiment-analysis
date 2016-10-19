2c1f866fe5bd56c85694c101cebfe02ef7ec843b
Tom Manion <thomaswmanion@gmail.com>
2016-10-19 17:29:43 -0500

    Changing to just price and sentiment

--- shared/variables.ts
+++ shared/variables.ts
@@ -1,17 +1,17 @@
 export class Variables {
     static priceThreshold: number = 3;
     static numDays: number = 2;
-    static numPreviousDaySentiments: number = Variables.numDays * 7;
+    static numPreviousDaySentiments: number = Variables.numDays * 10;
     static rbfsigma: number = 0.5;
     static C: number = 1.0;
 
     static includeSentimentChange: boolean = false;
-    static includePriceChange: boolean = false;
+    static includePriceChange: boolean = true;
     static includeNumTweetsChange: boolean = false;
     static includeSentiment: boolean = true;
     static includePrice: boolean = false;
     static includePriceBracket: boolean = false;
-    static includeNumTweets: boolean = true;
+    static includeNumTweets: boolean = false;
 
     //Time
     static includeTime: boolean = false;
@@ -20,8 +20,8 @@ export class Variables {
     //Fundamentals
     static includeHighChange: boolean = false;
     static includeLowChange: boolean = false;
-    static includeVolumeChange: boolean = true;
-    static includeDayOfWeek: boolean = true;
+    static includeVolumeChange: boolean = false;
+    static includeDayOfWeek: boolean = false;
 
     static kernelType: string = 'RBF';
     static svmType: string = 'C_SVC';
