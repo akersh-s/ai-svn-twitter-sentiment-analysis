cc35ab2c5f39c19f85237a81d6638ee3f177f775
Tom Manion <thomaswmanion@gmail.com>
2016-10-26 22:15:14 -0500

    Removing akersh for now

--- buy.sh
+++ buy.sh
@@ -1,5 +1,5 @@
 #Read buy.json and send in requests based on buy power.
 ts-node cancel/canceller.ts --username='tomskytwo' --password='Bigapples1!'
 ts-node buyer/index.ts --username='tomskytwo' --password='Bigapples1!'
-ts-node cancel/canceller.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
-ts-node buyer/index.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
+#ts-node cancel/canceller.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
+#ts-node buyer/index.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
--- sell.sh
+++ sell.sh
@@ -1,5 +1,5 @@
 #Sell stocks owned based on the isReadyToSell function of the SellSymbol class.
 ts-node cancel/canceller.ts --username='tomskytwo' --password='Bigapples1!' 
 ts-node seller/index.ts --username='tomskytwo' --password='Bigapples1!'
-ts-node cancel/canceller.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
-ts-node seller/index.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
+#ts-node cancel/canceller.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
+#ts-node seller/index.ts --username='akersh.s@gmail.com' --password='Iambond,jamesbond'
