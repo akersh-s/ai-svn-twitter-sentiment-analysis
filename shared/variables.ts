2643d9f761e7e11f151050722d1ed790bfc2b7fb
Tom Manion <thomaswmanion@gmail.com>
2016-12-08 18:48:38 -0600

    Alt

--- shared/variables.ts
+++ shared/variables.ts
@@ -8,7 +8,7 @@ export class Variables {
     static numPreviousDaySentiments: number = 6; //Variables.numDays * 2; //argInt('previous-day-sentiments', 6);
 
     static includeSub: boolean = argBoolean('include-sub', true);
-    static numPreviousDaySentimentsSub: number = Variables.numPreviousDaySentiments / 2; //argInt('previous-day-sentiments-sub', 3);
+    static numPreviousDaySentimentsSub: number = Variables.numPreviousDaySentiments; //argInt('previous-day-sentiments-sub', 3);
     static skipDaySentiments: number = argInt('skip-day-sentiments', 1);
 
     static includeNumTweetsChange: boolean = argBoolean('include-num-tweets-change', false);
