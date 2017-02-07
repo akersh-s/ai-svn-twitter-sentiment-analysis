5a86165445f67e5962bde5d54852ab2745e4c74c
Tom Manion <thomaswmanion@gmail.com>
2017-02-07 14:34:03 -0600

    Updating review

--- earnings/review.ts
+++ earnings/review.ts
@@ -3,14 +3,15 @@ import { formatDate, getDaysAgo, today, oneDay } from '../shared/util/date-util'
 import { changePercent } from '../shared/util/math-util';
 const threshold = +today - (oneDay * 10);
 const th = TradeHistory.readHistory();
-
+const reported: any = {};
 const todaysSells = th.filter(t => t.action === 'sell' && +t.date > threshold);
 
 todaysSells.forEach((todaySell) => {
     const equivBuy = findRelatedBuy(th.filter(t => t.stock === todaySell.stock && todaySell.date > t.date));
-    if (equivBuy) {
+    if (equivBuy && !reported[todaySell.stock + formatDate(todaySell.date)]) {
         const increaseAmount = changePercent(todaySell.price, equivBuy.price);
-        console.log(`Sold ${todaySell.quantity} shares of ${todaySell.stock} on ${formatDate(todaySell.date)}, likely purchased at ${equivBuy.price} on ${formatDate(equivBuy.date)}, Increased: %${increaseAmount.toFixed(2)}`);
+        console.log(`Sold ${todaySell.quantity} shares of ${todaySell.stock} at $${todaySell.price.toFixed(2)} on ${formatDate(todaySell.date)}, likely purchased at $${equivBuy.price.toFixed(2)} on ${formatDate(equivBuy.date)}, Percent Changed: %${increaseAmount.toFixed(2)}`);
+        reported[todaySell.stock + formatDate(todaySell.date)] = true;
     }
 });
 
