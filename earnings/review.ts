9ea5f60e106c6aca179961ca796107a11faefb67
Tom Manion <thomaswmanion@gmail.com>
2017-02-06 22:29:28 -0600

    Woops

--- earnings/review.ts
+++ earnings/review.ts
@@ -2,16 +2,8 @@ import { TradeHistory } from '../shared/trade-history.model';
 import { formatDate, getDaysAgo, today, oneDay } from '../shared/util/date-util';
 import { changePercent } from '../shared/util/math-util';
 const threshold = +today - (oneDay * 10);
-//const th = TradeHistory.readHistory();
-const th = [
-    new TradeHistory('buy', 'GOOG', 5, 90, new Date('10/27/2016')),
-    new TradeHistory('sell', 'GOOG', 5, 100, new Date('10/22/2016')),
-    new TradeHistory('buy', 'GOOG', 5, 90, new Date('10/21/2016')),
-    new TradeHistory('buy', 'GOOG', 5, 90, new Date('10/20/2016')),
-    new TradeHistory('sell', 'GOOG', 5, 100, new Date('10/19/2016')),
-    new TradeHistory('buy', 'GOOG', 5, 10, new Date('10/18/2016')),
-    new TradeHistory('buy', 'GOOG', 5, 90, new Date('10/17/2016'))
-]
+const th = TradeHistory.readHistory();
+
 const todaysSells = th.filter(t => t.action === 'sell' && +t.date > threshold);
 todaysSells.forEach((todaySell) => {
     const equivBuy = findRelatedBuy(th.filter(t => t.stock === todaySell.stock && todaySell.date > t.date));
